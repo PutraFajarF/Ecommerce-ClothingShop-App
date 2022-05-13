38a14c283463e76ac3f161f41a001fef4db04767
Putra Fajar <putrautalls@gmail.com>
2022-05-13 23:54:53 +0700

    add shop and signin route use Link from ReactRouterDOM & add Firebase library to my project

--- src/routes/home/home.component.jsx
+++ src/routes/home/home.component.jsx
@@ -1,3 +1,4 @@
+import { Outlet } from "react-router-dom";
 import Directory from "../../components/directory/directory.component";
 
 const Home = () => {
@@ -29,7 +30,10 @@ const Home = () => {
     }
   ];
   return (
-    <Directory categories={categories}/>
+    <div>
+      <Directory categories={categories}/>
+      <Outlet />
+    </div>
   );
 };
 
