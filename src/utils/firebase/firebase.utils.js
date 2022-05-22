aab37054931f0c3126d045c84d9f6d95d8c5fe1f
Putra Fajar <putrautalls@gmail.com>
2022-05-23 00:45:36 +0700

    fully migrated from Redux-Thunk to Redux-Saga for handle side effects state management

--- src/utils/firebase/firebase.utils.js
+++ src/utils/firebase/firebase.utils.js
@@ -76,7 +76,7 @@ export const createUserDocumentFromAuth = async (userAuth, additionalInformation
     }
   }
   // Check if user data exists => return userDocRef
-  return userDocRef;
+  return userSnapshot;
 };
 
 export const createAuthUserWithEmailAndPassword = async (email, password) => {
@@ -91,4 +91,18 @@ export const signInAuthUserWithEmailAndPassword = async (email, password) => {
 
 export const signOutUser = async () => await signOut(auth);
 
-export const onAuthStateChangedListener = (callback) => onAuthStateChanged(auth, callback);
+export const onAuthStateChangedListener = (callback) => 
+  onAuthStateChanged(auth, callback);
+
+export const getCurrentUser = () => {
+  return new Promise((resolve, reject) => {
+    const unsubscribe = onAuthStateChanged(
+      auth,
+      (userAuth) => {
+        unsubscribe();
+        resolve(userAuth);
+      },
+      reject
+    );
+  });
+};
