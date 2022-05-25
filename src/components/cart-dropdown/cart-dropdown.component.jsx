26dd89d12e23e747372990799599228334a43a94
Putra Fajar <putrautalls@gmail.com>
2022-05-25 23:57:52 +0700

    migrated cart store jsx to typescript

--- src/components/cart-dropdown/cart-dropdown.component.jsx
+++ src/components/cart-dropdown/cart-dropdown.component.jsx
@@ -3,7 +3,7 @@ import { useNavigate } from 'react-router-dom';
 import Button from '../button/button.component';
 import CartItem from '../cart-item/cart-item.component';
 import { selectCartItems } from '../../store/cart/cart.selector';
-import { CartDropdownContainer, EmptyMessage, CartItems } from './cart-dropdown.styles.jsx';
+import { CartDropdownContainer, EmptyMessage, CartItems } from './cart-dropdown.styles';
 
 const CartDropdown = () => {
   const cartItems = useSelector(selectCartItems);
--- src/store/categories/category.reducer.ts
+++ src/store/categories/category.reducer.ts
@@ -7,7 +7,7 @@ export type CategoriesState = {
   readonly categories: Category[];
   readonly isLoading: boolean;
   readonly error: Error | null;
-}
+};
 
 export const CATEGORIES_INITIAL_STATE: CategoriesState = {
   categories: [],
@@ -15,7 +15,7 @@ export const CATEGORIES_INITIAL_STATE: CategoriesState = {
   error: null,
 };
 
-export const categoriesReducer = (state = CATEGORIES_INITIAL_STATE, action = {} as AnyAction): CategoriesState => {
+export const categoriesReducer = (state = CATEGORIES_INITIAL_STATE, action: AnyAction): CategoriesState => {
   if (fetchCategoriesStart.match(action)) {
     return { ...state, isLoading: true }; 
   }
